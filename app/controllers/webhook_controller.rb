65ea8505203b92a24b5886e93abbb75e651014b5
kenzan8000 <kenzan8000@gmail.com>
2016-04-14 11:17:30 -0700

    update webhook controller

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -7,26 +7,27 @@ class WebhookController < ApplicationController
 
   def get_facebook
     verify_token = params['hub.verify_token']
-    render :status => 401 and return unless verify_token == ENV['FB_VERIFY_TOKEN']
+    render 'no verify_token' and return unless verify_token == ENV['FB_VERIFY_TOKEN']
     challenge = params['hub.challenge']
-    render :status => 401 and return unless challenge
+    render 'no challenge' and return unless challenge
 
-    render json: challenge
+    render challenge
   end
 
 
   def post_facebook
     message = params['entry'][0]['messaging'][0]
+    json = {}
     if message.include?('message')
 
       sender_id = message['sender']['id']
       text = message['message']['text']
 
       facebook_client = FacebookClient.new
-      facebook_client.post_message(sender_id, text)
+      json = facebook_client.post_message(sender_id, text)
     end
 
-    render :status => 200
+    render json: json
   end
 
 
