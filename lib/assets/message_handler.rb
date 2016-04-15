1059b6adbbbaa0c0c790642e7901ab2885b461c7
kenzan8000 <kenzan8000@gmail.com>
2016-04-15 15:14:18 -0700

    test

--- lib/assets/message_handler.rb
+++ lib/assets/message_handler.rb
@@ -197,15 +197,17 @@ class MessageHandler
       #  @sender.current_step_id = @sender.steps[index].id and break
       #end
 
-      steps = Step.where(sender_id: @sender.id)
-      for i in 0..steps.count
-        next unless @sender.current_step_id == steps[i].id
-        index = i + 1
-        # next navigation status
-        @sender.navigation_status += 1 and break if index >= steps.count
-        # next step
-        @sender.current_step_id = steps[index].id and break
-      end
+      #steps = Step.where(sender_id: @sender.id)
+      #for i in 0..steps.count
+      #  next unless @sender.current_step_id == steps[i].id
+      #  index = i + 1
+      #  # next navigation status
+      #  @sender.navigation_status += 1 and break if index >= steps.count
+      #  # next step
+      #  @sender.current_step_id = steps[index].id and break
+      #end
+
+      @sender.current_step_id = @sender.steps.last.id
 
       @sender.save if @sender.valid?
     end
